9d19eb08453115fda3a910c3fc7e8b6856f3ce6b
“abdessamed” <aitlhaj.abdessamed@gmail.com>
2023-11-26 13:15:04 -0800

    practice using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # SSH confiiguration file
-file_line {'no pass'
+file_line {'no pass':
   ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentification no'
 }
 
-file_line {'using specific identity'
+file_line {'using specific identity':
   ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school'
