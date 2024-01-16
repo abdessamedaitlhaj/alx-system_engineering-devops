4222a0f265728993f7f3b1af7edcf0ee5040dfaa
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-16 09:50:07 -0800

    apis

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -14,8 +14,12 @@ if __name__ == "__main__":
         url + "users/" + user_id + "/todos/").text)
 
     file_name = user_id + ".JSON"
-    with open(file_name, mode='w', newline='') as f:
-        data = {user_id: [{"task": task.get("title"),
-                "completed": task.get("completed"), "username": user.get(
-                "username")} for task in todolist]}
-        json.dump(data, f)
+    with open(file_name, mode='w') as f:
+        data = [
+                {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user.get(
+                "username")} for task in todolist
+                ]
+        json.dump({user_id : data}, f)
