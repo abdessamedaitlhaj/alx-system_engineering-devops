ae3e6d0c37e3f3399ab075eb4077acdb29bfb066
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-16 09:05:02 -0800

    apis

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""export data in the CSV format"""
+
+import json
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user = json.loads(requests.get(url + "users/" + user_id).text)
+    todolist = json.loads(requests.get(
+        url + "users/" + user_id + "/todos/").text)
+
+    file_name = user_id + ".csv"
+    with open(file_name, mode='w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todolist:
+            writer.writerow(
+                [user_id, user.get("username"),
+                    task.get("completed"), task.get("title")]
+                    )
