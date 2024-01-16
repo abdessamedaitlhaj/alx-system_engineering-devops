61becf1308d449593e047f43270856f8797e0164
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-16 08:37:10 -0800

    apis

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,22 +5,23 @@ import json
 import requests
 import sys
 
-user_id = sys.argv[1]
-url = "https://jsonplaceholder.typicode.com/"
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
 
-user = json.loads(requests.get(url + "users/" + user_id).text)
-todolist = json.loads(requests.get(url + "todos/").text)
+    user = json.loads(requests.get(url + "users/" + user_id).text)
+    todolist = json.loads(requests.get(url + "todos/").text)
 
-count = 0
-t_tasks = 0
-for task in todolist:
-    if task.get("completed") and task.get("userId") == user.get("id"):
-        count += 1
-    if task.get("userId") == user.get("id"):
-        t_tasks += 1
+    count = 0
+    t_tasks = 0
+    for task in todolist:
+        if task.get("completed") and task.get("userId") == user.get("id"):
+            count += 1
+        if task.get("userId") == user.get("id"):
+            t_tasks += 1
 
-print("Employee {} is done with tasks({}/{}):".format(
-    user.get("name"), count, t_tasks))
-for task in todolist:
-    if task.get("completed") and task.get("userId") == user.get("id"):
-        print(f"\t {task.get('title')}")
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), count, t_tasks))
+    for task in todolist:
+        if task.get("completed") and task.get("userId") == user.get("id"):
+            print(f"\t {task.get('title')}")
