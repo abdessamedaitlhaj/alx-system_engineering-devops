7e36238953f72a92439743c7f8328d4744df8a17
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-16 09:20:10 -0800

    apis

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""export data in the JSON format"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user = json.loads(requests.get(url + "users/" + user_id).text)
+    todolist = json.loads(requests.get(
+        url + "users/" + user_id + "/todos/").text)
+
+    file_name = user_id + ".JSON"
+    with open(file_name, mode='w', newline='') as f:
+        data = {user_id: [{"task": task.get("title"),
+                "completed": task.get("completed"), "username": user.get(
+                "username")} for task in todolist]}
+        json.dump(data, f)
