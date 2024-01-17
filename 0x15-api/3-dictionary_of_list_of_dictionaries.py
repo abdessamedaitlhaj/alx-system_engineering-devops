c9bfad3412edbe84a858244e91249ea4b1bb36e1
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-17 05:39:10 -0800

    apis

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""export all reacords of all employees to the JSON format"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = "https://jsonplaceholder.typicode.com/"
+
+    users = json.loads(requests.get(url + "users/").text)
+    todolist = json.loads(requests.get(
+        url + "todos/").text)
+    file_name = "todo_all_employees.json"
+    with open(file_name, mode='w') as f:
+        data = []
+        all_employees = {}
+        for user in users:
+            all_employees.update({user.get("id"): [
+                {
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": user.get("username"),
+                    }
+                for task in todolist
+                ]})
+        json.dump(all_employees, f)
