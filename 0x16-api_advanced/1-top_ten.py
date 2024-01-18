0a4faed7c477c5d9f33cbd4d91a649ec7ec13df0
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-18 08:54:27 -0800

    api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,15 +7,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return number of subscriber of a given subreddit"""
+    """Return top 10 new post for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "GetSubscribers/1.0 by /u/abdessamed"}
+    headers = {"User-Agent": "GetTenNewPosts/1.0 by /u/abdessamed"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        for i in range(10):
+            print(my_data["data"]["children"][i]["title"])
     else:
         return 0
