a192dae5b10605803ad6bc6a4f5b7ec4c9dba4a0
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-18 16:30:43 -0800

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,12 @@ def number_of_subscribers(subreddit):
     if not subreddit:
         return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    #headers = {"User-Agent": "GetSubscribers/1.0 by /u/abdessamed"}
     headers = {'User-Agent': 'GetSubscribers'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         try:
             data = response.json()
-            return data["data"]["subscribers"]
+            return data.get("data").get("subscribers")
         except KeyError:
             return 0
     else:
