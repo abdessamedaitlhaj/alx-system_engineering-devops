84ed9a644a5e973ace8a7abdc5200ff3506d660b
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-18 16:27:43 -0800

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return number of subscriber of a given subreddit"""
+    if not subreddit:
+        return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     #headers = {"User-Agent": "GetSubscribers/1.0 by /u/abdessamed"}
-    headers = {'User-Agent': 'android:com.example.myredditapp:v1.2.3 (by /u/kemitche)'}
+    headers = {'User-Agent': 'GetSubscribers'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         try:
