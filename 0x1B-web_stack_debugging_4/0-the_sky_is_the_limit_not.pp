28ff3a601ea988de41d8bd3161b934c5aee7e32a
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2024-01-25 12:44:27 -0800

    stack 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# resolution of limit requests
+exec { 'ncrease-limit':
+  command => 'sed -i "s/15/8192/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
